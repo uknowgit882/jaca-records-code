2859e1d8260cbb0ae1b15e9dfef12a92ced206d1
Aseel Tungekar <aseelt@hotmail.com>
2023-12-03 17:17:04 -0500

    api client service working (unauthenticated)

---
+++ dotnet/Capstone/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using Capstone.Models;
+using Capstone.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public readonly IRecordService recordService = new RecordService();
+        public TestController() { }
+
+        [HttpGet("GetRecord/{recordId}")]
+        public ActionResult<Record> GetRecordById(int recordId)
+        {
+            try
+            {
+                Record output = null;
+                output = recordService.GetRecord(recordId);
+                if(output != null)
+                {
+                    return Ok(output);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
