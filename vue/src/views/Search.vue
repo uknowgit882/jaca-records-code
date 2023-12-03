bd18515fc9787ab0964a4d63e5cae69d1d1df915
Aliz Gosztola <aliz.gosztola@gmail.com>
2023-12-03 16:25:36 -0500

    Added background, search.vue - search bar, advanced search button

---
+++ vue/src/views/Search.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div>
+  <div class="field has-addons">
+  <div class="control">
+    <input class="input is-large is-hovered" type="text" placeholder="Find a Record, Artist etc.">
+  </div>
+  <div class="control">
+    <a class="button is-info is-large">
+      Search
+    </a>
+  </div>
+</div>
+<button class="button">Advanced Search</button>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
