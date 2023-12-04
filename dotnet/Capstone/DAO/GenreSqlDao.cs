6009cc1b20b9cddaab69b81e189a294b854a6c22
Caleb Abrams <calebabrams97@gmail.com>
2023-12-04 14:44:51 -0500

    CalebsDaoForNow

--- dotnet/Capstone/DAO/GenreSqlDao.cs
+++ dotnet/Capstone/DAO/GenreSqlDao.cs
@@ -1,4 +1,8 @@
-﻿namespace Capstone.DAO
+﻿using Capstone.Models;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace Capstone.DAO
 {
     public class GenreSqlDao : IGenresDao
     {
@@ -11,7 +15,31 @@
 
         public string GetGenre(string genre)
         {
-            
+            string output = null;
+            string sql = "SELECT genre_id, name, is_active, created_date, updated_date FROM genres " +
+                "WHERE name = @name;";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@name", genre);
+
+                    SqlDataAdapter reader = cmd.ExecuteReader();
+
+                    if (reader.read())
+                    {
+                        output = MapToRowGenre(reader);
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
             return genre;
         }
         public bool AddGenre(string genre)
@@ -19,5 +47,9 @@
             return true;
         }
 
+        private string MapToRowGenre(SqlDataReader reader)
+        {
+            
+        }
     }
 }
