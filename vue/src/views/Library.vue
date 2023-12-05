bbcfd7fed7098577dba268370655510eb0a03a80
Aliz Gosztola <aliz.gosztola@gmail.com>
2023-12-04 21:25:31 -0500

    Added Libaray.vue, made mock data appear

---
+++ vue/src/views/Library.vue
@@ -0,0 +1,29 @@
+<template>
+    
+    <div class = "Library-container">
+        <record-in-library v-for="record in $store.state.records" v-bind:record="record" v-bind:key="record.id"/>
+    
+    </div>
+    
+</template>
+
+<script>
+import RecordInLibrary from './RecordInLibrary.vue';
+
+
+export default{
+    components: {
+        RecordInLibrary
+    }
+}
+</script>
+
+<style scoped>
+.Library-container{
+    display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  background-color: black;
+}
+
+</style>
