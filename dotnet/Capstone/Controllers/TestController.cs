3a9a6f47752bfdd0a7711240cc44e381c8ead140
Caleb Abrams <calebabrams97@gmail.com>
2023-12-05 17:35:44 -0500

    Attempted fix

--- dotnet/Capstone/Controllers/TestController.cs
+++ dotnet/Capstone/Controllers/TestController.cs
@@ -101,106 +101,116 @@ namespace Capstone.Controllers
                         newRecord = _recordBuilderDao.AddRecord(clientSuppliedRecord);
                     }
 
-        [HttpPost("Artist/")]
-        public ActionResult<bool> AddArtist(Artist newArtist)
-        {
-            try
-            {
-                bool output = false;
-                output = _artistsDao.AddArtist(newArtist);
-                // redirects when created (placeholder), or gives a 200 and message
-                return output ? Created("https://localhost:44315/", output) : Ok("This artist already exists");
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-        [HttpPost("Format/")]
-        public ActionResult<bool> AddFormat(string newFormat)
-        {
-            try
-            {
-                bool output = false;
-                output = _formatsDao.AddFormat(newFormat);
-                return output ? Created("https://localhost:44315/", output) : Ok("This format already exists");
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-        [HttpGet("Friend/ById/{id}")]
-        public ActionResult<UserAndFriends> GetUsersFriendsById(int id)
-        {
-            try
-            {
-                UserAndFriends output = null;
-                output = _friendsDao.GetUsersFriendsById(id);
-                if (output != null)
-                {
-                    return Ok(output);
+                    // build out the preceding primary tables
+                    // if count is zero, no need for the sql dao for this particular table
+                    if (clientSuppliedRecord.Genres.Count != 0)
+                    {
+                        foreach (string genre in clientSuppliedRecord.Genres)
+                        {
+                            _genresDao.AddGenre(genre);
+                            Genre genreReturned = _genresDao.GetGenre(genre);
+                            _recordsGenresDao.AddRecordGenre(clientSuppliedRecord.Id, genreReturned.Genre_Id);
+                        }
+                    }
+                    if (clientSuppliedRecord.Styles.Count != 0)
+                    {
+                        // we're just lumping genres and styles (sub genre it seems) into one
+                        // so repeate the method call
+                        foreach (string style in clientSuppliedRecord.Styles)
+                        {
+                            _genresDao.AddGenre(style);
+                            Genre genreReturned = _genresDao.GetGenre(style);
+                            _recordsGenresDao.AddRecordGenre(clientSuppliedRecord.Id, genreReturned.Genre_Id);
+                        }
+                    }
+                    if (clientSuppliedRecord.Labels.Count != 0)
+                    {
+                        foreach (Label item in clientSuppliedRecord.Labels)
+                        {
+                            _labelsDao.AddLabel(item);
+                            Label returnedLabel = _labelsDao.GetLabel(item);
+                            _recordsLabelsDao.AddRecordLabel(clientSuppliedRecord.Id, returnedLabel.Label_Id);
+                        }
+                    }
+                    if (clientSuppliedRecord.Formats.Count != 0)
+                    {
+                        foreach (Format item in clientSuppliedRecord.Formats)
+                        {
+                            // want only the vinyl formats...
+                            if (item.Name.ToLower() == "vinyl")
+                            {
+                                // then go through the descriptions array and add it to the format table
+                                foreach (string description in item.Descriptions)
+                                {
+                                    _formatsDao.AddFormat(description);
+                                    Format returnedFormat = _formatsDao.GetFormat(description);
+                                    _recordsFormatsDao.AddRecordFormat(clientSuppliedRecord.Id, returnedFormat.Format_Id);
+                                }
+                            }
+                        }
+                    }
+                    if (clientSuppliedRecord.Artists.Count != 0)
+                    {
+                        foreach (Artist artist in clientSuppliedRecord.Artists)
+                        {
+                            _artistsDao.AddArtist(artist);
+                            Artist returnedArtist = _artistsDao.GetArtist(artist);
+                            _recordsArtistsDao.AddRecordArtist(clientSuppliedRecord.Id, returnedArtist.Artist_Id);
+                        }
+                    }
+                    if (clientSuppliedRecord.ExtraArtists.Count != 0)
+                    {
+                        // same thing for extra artists
+                        foreach (Artist extraArtist in clientSuppliedRecord.ExtraArtists)
+                        {
+                            _artistsDao.AddArtist(extraArtist);
+                            Artist returnedArtist = _artistsDao.GetArtist(extraArtist);
+                            _recordsExtraArtistsDao.AddRecordExtraArtist(clientSuppliedRecord.Id, returnedArtist.Artist_Id);
+                        }
+                    }
+
+
+                    // then do the other downstream builds
+                    if (clientSuppliedRecord.Identifiers.Count != 0)
+                    {
+                        foreach (Identifier identifier in clientSuppliedRecord.Identifiers)
+                        {
+                            identifier.Discogs_Id = clientSuppliedRecord.Id;
+                            _barcodesDao.AddIdentifier(identifier);
+                        }
+                    }
+                    if (clientSuppliedRecord.Images.Count != 0)
+                    {
+                        foreach (Image image in clientSuppliedRecord.Images)
+                        {
+                            image.Discogs_Id = clientSuppliedRecord.Id;
+                            _imagesDao.AddImage(image);
+                        }
+                    }
+                    if (clientSuppliedRecord.Tracklist.Count != 0)
+                    {
+                        foreach (Track track in clientSuppliedRecord.Tracklist)
+                        {
+                            track.Discogs_Id = clientSuppliedRecord.Id;
+                            _tracksDao.AddTrack(track);
+                        }
+                    }
+                    // if you get here, assume that everything went well
+                    // activate the record
+                    // if you have an existing record, activate that
+                    // if you have a new record, activate that
+                    if (existingRecord != null)
+                    {
+                        _recordBuilderDao.ActivateRecord(existingRecord.Discogs_Id);
+                    }
+                    else
+                    {
+                        _recordBuilderDao.ActivateRecord(newRecord.Discogs_Id);
+                    }
+
+                    return Created("https://localhost:44315/", newRecord);
                 }
-                else
-                {
-                    return NotFound();
-                }
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-        [HttpGet("Friend/ByUsername/{username}")]
-        public ActionResult<UserAndFriends> GetUsersFriendsByUsername(string username)
-        {
-            try
-            {
-                UserAndFriends output = null;
-                output = _friendsDao.GetUsersFriendsByUsername(username);
-                if (output != null)
-                {
-                    return Ok(output);
-                }
-                else
-                {
-                    return NotFound();
-                }
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-        [HttpPost("Friend/{usersId}/Add/{friendsId}")]
-        public ActionResult<UserAndFriends> AddFriend(int usersId, int friendsId)
-        {
-            try
-            {
-                UserAndFriends output = null;
-                output = _friendsDao.AddFriend(usersId, friendsId);
-                if (output != null)
-                {
-                    return Created("https://localhost:44315/", output);
-                }
-                else
-                {
-                    return NotFound();
-                }
-            }
-            catch (Exception e)
-            {
-                return BadRequest("Something went wrong adding your friend. Perhaps you are already friends?");
-            }
-        }
-        [HttpDelete("Friend/{usersId}/Delete/{friendsId}")]
-        public ActionResult<UserAndFriends> DropFriend(int usersId, int friendsId)
-        {
-            try
-            {
-                UserAndFriends output = null;
-                output = _friendsDao.DropFriend(usersId, friendsId);
-                if (output != null)
+                else if (clientSuppliedRecord.Date_Changed != existingRecord.Discogs_Date_Changed)
                 {
                     //do an update
                     return Ok();
@@ -212,7 +222,7 @@ namespace Capstone.Controllers
                 }
 
             }
-            catch (Exception)
+            catch (Exception e)
             {
 
                 return BadRequest(e.Message);
@@ -259,7 +269,8 @@ namespace Capstone.Controllers
             else if (searchRequest.TypeOfSearch == "Library")
             {
 
-            } else if (searchRequest.TypeOfSearch == "Collections")
+            }
+            else if (searchRequest.TypeOfSearch == "Collections")
             {
 
             }
