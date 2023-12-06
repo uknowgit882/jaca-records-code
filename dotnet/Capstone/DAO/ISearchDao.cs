9abe39785135d6f90fd8c852a4fd8f79d1c04377
Jake Leya <jleya95@gmail.com>
2023-12-06 13:22:22 -0500

    Library wildcard search functional; added ISearchDao to test controller + dependency injection; Endpoint for Library wildcard search designed (only for testing wildcard SQL, no results generated yet)

--- dotnet/Capstone/DAO/ISearchDao.cs
+++ dotnet/Capstone/DAO/ISearchDao.cs
@@ -1,7 +1,10 @@
 ﻿using Capstone.Models;
+using System.Collections.Generic;
+
 namespace Capstone.DAO
 {
     public interface ISearchDao
     {
+        public List<int> WildcardSearchDatabaseForRecords(SearchRequest requestObject);
     }
 }
--- dotnet/Capstone/DAO/SearchSqlDao.cs
+++ dotnet/Capstone/DAO/SearchSqlDao.cs
@@ -6,10 +6,10 @@ using System.Data.SqlClient;
 
 namespace Capstone.DAO
 {
-    public class SearchDao: ISearchDao
+    public class SearchSqlDao : ISearchDao
     {
         private readonly string connectionString;
-        public SearchDao(string dbConnectionString)
+        public SearchSqlDao(string dbConnectionString)
         {
             connectionString = dbConnectionString;
         }
@@ -22,21 +22,18 @@ namespace Capstone.DAO
                 "FROM records " +
                 "JOIN records_artists ON records.discogs_id = records_artists.discogs_id " +
                 "JOIN artists ON records_artists.artist_id = artists.artist_id " +
-                "JOIN records_extra_artists ON records.discogs_id = records_extra_artists.discogs_id " +
-                "JOIN artists AS extra_artists ON records_extra_artists.extra_artist_id = extra_artists.artist_id" +
-                "JOIN images ON records.discogs_id = images.discogs_id " +
                 "JOIN records_labels ON records.discogs_id = records_labels.discogs_id " +
                 "JOIN labels ON records_labels.label_id = labels.label_id " +
-                "JOIN tracks ON records.discogs_id = tracks.discogs_id " +
                 "JOIN barcodes ON records.discogs_id = barcodes.discogs_id " +
-                "JOIN records_formats ON records.discogs_id = records_formats.discogs_id " +
-                "JOIN formats ON records_formats.format_id = formats.format_id " +
                 "JOIN records_genres ON records.discogs_id = records_genres.discogs_id " +
                 "JOIN genres ON records_genres.genre_id = genres.genre_id " +
-                "WHERE record.title LIKE @recordTitle AND artist.name LIKE @artistName " +
-                "AND genre.name LIKE @genreName AND records.released LIKE @recordsReleased " +
-                "AND record.country LIKE @recordCountry AND labels.name LIKE @labelsName " +
-                "AND barcode.value LIKE @barcodeValue";
+                "WHERE (records.title LIKE @recordsTitle OR @recordsTitle = '') " +
+                "AND (artists.name LIKE @artistsName OR @artistsName = '') " +
+                "AND (genres.name LIKE @genresName OR @genresName = '') " +
+                "AND (records.released LIKE @recordsReleased OR @recordsReleased = '') " +
+                "AND (records.country LIKE @recordsCountry OR @recordsCountry = '') " +
+                "AND (labels.name LIKE @labelsName OR @labelsName = '') " +
+                "AND (barcodes.value LIKE @barcodesValue OR @barcodesValue = '')";
             try
             {
                 using (SqlConnection conn = new SqlConnection(connectionString))
@@ -44,26 +41,30 @@ namespace Capstone.DAO
                     conn.Open();
 
                     SqlCommand cmd = new SqlCommand(sql, conn);
-                    cmd.Parameters.AddWithValue("@recordTitle", SearchStringWildcardAdder(requestObject.Title));
-                    cmd.Parameters.AddWithValue("@artistName", SearchStringWildcardAdder(requestObject.Artist));
-                    cmd.Parameters.AddWithValue("@genreName", SearchStringWildcardAdder(requestObject.Genre));
+                    cmd.Parameters.AddWithValue("@recordsTitle", SearchStringWildcardAdder(requestObject.Title));
+                    cmd.Parameters.AddWithValue("@artistsName", SearchStringWildcardAdder(requestObject.Artist));
+                    cmd.Parameters.AddWithValue("@genresName", SearchStringWildcardAdder(requestObject.Genre));
                     cmd.Parameters.AddWithValue("@recordsReleased", SearchStringWildcardAdder(requestObject.Year));
-                    cmd.Parameters.AddWithValue("@recordCountry", SearchStringWildcardAdder(requestObject.Country));
+                    cmd.Parameters.AddWithValue("@recordsCountry", SearchStringWildcardAdder(requestObject.Country));
                     cmd.Parameters.AddWithValue("@labelsName", SearchStringWildcardAdder(requestObject.Label));
-                    cmd.Parameters.AddWithValue("@barcodeValue", SearchStringWildcardAdder(requestObject.Barcode));
+                    cmd.Parameters.AddWithValue("@barcodesValue", SearchStringWildcardAdder(requestObject.Barcode));
                     SqlDataReader reader = cmd.ExecuteReader();
 
-                    //if (reader.Read())
-                    //{
-                    //    recordIDs;
-                    //}
+                    while (reader.Read())
+                    {
+                        int idToAdd = Convert.ToInt32(reader["discogs_id"]);
+                        if (!recordIDs.Contains(idToAdd))
+                        {
+                            recordIDs.Add(idToAdd);
+                        }
+                    }
                 }
             }
             catch (SqlException ex)
             {
                 throw new DaoException("Sql exception occured", ex);
             }
-            
+
 
             return recordIDs;
         }
