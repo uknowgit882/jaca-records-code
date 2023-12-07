830ffd406a8e4b5682e1ae7e6aee1eee001c248c
Aseel Tungekar <aseelt@hotmail.com>
2023-12-07 15:48:29 -0500

    got some endpoints working

---
+++ dotnet/Capstone/Controllers/JacapremeController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capstone.Exceptions;
+using Capstone.Models;
+using Capstone.Security;
+using Capstone.DAO.Interfaces;
+using System;
+using Capstone.DAO;
+using Capstone.Service;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [Authorize(Roles = "jacapreme")]
+    [ApiController]
+    public class JacapremeController : CommonController
+    {
+        public JacapremeController(IArtistsDao artistsDao, IBarcodesDao barcodesDao, IFormatsDao formatsDao, IFriendsDao friendsDao, IGenresDao genresDao, IImagesDao imagesDao, ILabelsDao labelsDao, IRecordBuilderDao recordBuilderDao, IRecordsArtistsDao recordsArtistsDao, IRecordsExtraArtistsDao recordsExtraArtistsDao, IRecordsFormatsDao recordsFormatsDao, IRecordsGenresDao recordsGenresDao, IRecordsLabelsDao recordsLabelsDao, IRecordService recordService, ITracksDao tracksDao, IUserDao userDao, ISearchDao searchDao) : base(artistsDao, barcodesDao, formatsDao, friendsDao, genresDao, imagesDao, labelsDao, recordBuilderDao, recordsArtistsDao, recordsExtraArtistsDao, recordsFormatsDao, recordsGenresDao, recordsLabelsDao, recordService, tracksDao, userDao, searchDao)
+        {
+        }
+        
+        // only admins should be able to reactivate
+        [HttpPut("userfunctions/reactivate/{username}")]
+        public ActionResult<string> ReactivateUser(string username)
+        {
+            // don't need authentication that it matches the user to action
+            // we're the admins, it won't
+            if (string.IsNullOrEmpty(username))
+            {
+                // validation of the input
+                // if fails, stop
+                return BadRequest("You must enter a valid username");
+            }
+
+            try
+            {
+                bool output = _userDao.ReactivateUser(username);
+                if (output)
+                {
+                    return Ok($"{username} was reactivated");
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut("userfunctions/upgradetoadmin/{username}")]
+        public ActionResult<string> UpgradeUserToAdmin(string username)
+        {
+            // don't need authentication that it matches the user to action
+            // we're the admins, it won't
+            if (string.IsNullOrEmpty(username))
+            {
+                // validation of the input
+                // if fails, stop
+                return BadRequest("You must enter a valid username");
+            }
+
+            try
+            {
+                bool output = _userDao.UpgradeAdmin(username);
+                if (output)
+                {
+                    return Ok($"{username} was reactivated");
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut("userfunctions/downgradefromadmin/{username}")]
+        public ActionResult<string> DowngradeAdmin(string username)
+        {
+            // don't need authentication that it matches the user to action
+            // we're the admins, it won't
+            if (string.IsNullOrEmpty(username))
+            {
+                // validation of the input
+                // if fails, stop
+                return BadRequest("You must enter a valid username");
+            }
+
+            try
+            {
+                bool output = _userDao.DowngradeAdmin(username);
+                if (output)
+                {
+                    return Ok($"{username} was reactivated");
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ dotnet/Capstone/Controllers/Test Controllers/AlizTestController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capstone.Exceptions;
+using Capstone.Models;
+using Capstone.Security;
+using Capstone.DAO.Interfaces;
+using System;
+using Capstone.DAO;
+using Capstone.Service;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AlizTestController : CommonController
+    {
+        public AlizTestController(IArtistsDao artistsDao, IBarcodesDao barcodesDao, IFormatsDao formatsDao, IFriendsDao friendsDao, IGenresDao genresDao, IImagesDao imagesDao, ILabelsDao labelsDao, IRecordBuilderDao recordBuilderDao, IRecordsArtistsDao recordsArtistsDao, IRecordsExtraArtistsDao recordsExtraArtistsDao, IRecordsFormatsDao recordsFormatsDao, IRecordsGenresDao recordsGenresDao, IRecordsLabelsDao recordsLabelsDao, IRecordService recordService, ITracksDao tracksDao, IUserDao userDao, ISearchDao searchDao) : base(artistsDao, barcodesDao, formatsDao, friendsDao, genresDao, imagesDao, labelsDao, recordBuilderDao, recordsArtistsDao, recordsExtraArtistsDao, recordsFormatsDao, recordsGenresDao, recordsLabelsDao, recordService, tracksDao, userDao, searchDao)
+        {
+        }
+        // Sample, please edit as needed
+        [HttpPut("userfunctions/{username}")]
+        public ActionResult<string> DeactivateUser(string username)
+        {
+            // check if you have a valid value, and it matches the user who is logged in (for security)
+            // so they are actioning their own profile
+            if (string.IsNullOrEmpty(username) || User.Identity.Name != username)
+            {
+                // validation of the input
+                // if fails, stop
+                return BadRequest("You must enter a valid username");
+            }
+
+            try
+            {
+                bool output = _userDao.DeactivateUser(username);
+                if (output)
+                {
+                    return Ok($"{username} was deactivated");
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
