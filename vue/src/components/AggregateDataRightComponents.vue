8252ae4b48569d04dd3f63d34280fe6049514574
Aliz Gosztola <aliz.gosztola@gmail.com>
2023-12-11 12:36:00 -0500

    Added aggregate data for users as well

---
+++ vue/src/components/AggregateDataRightComponents.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="AggData">
+        <div class="AggDataRight">
+            <h2> Total Artists: {{ this.$store.state.StatsUser.totalArtists }}</h2>
+            <h2> Total Collections: {{ this.$store.state.StatsUser.totalCollections }}</h2>
+            <h2> Total Formats: {{ this.$store.state.StatsUser.totalFormats }}</h2>
+            <h2> Total Genres: {{ this.$store.state.StatsUser.totalGenres }}</h2>
+            <h2> Total Images: {{ this.$store.state.StatsUser.totalImages }}</h2>
+            <h2> Total Labels: {{ this.$store.state.StatsUser.totalLabels }}</h2>
+            <h2> Total Records in Collections: {{ this.$store.state.StatsUser.totalRecordsInCollections }}</h2>
+            <h2> Total Tracks: {{ this.$store.state.StatsUser.totalTracks }}</h2>
+            <h2 v-for="(artist, numRecords) in $store.state.StatsUser.numRecordsByArtist" :key="artist">
+                Number of Records By Artist ({{ artist }}): {{ numRecords }}
+            </h2>
+            <h2 v-for="(genre, numRecords) in $store.state.StatsUser.numRecordsByGenre" :key="genre">
+                Number of Records By Genre ({{ genre }}): {{ numRecords }}
+            </h2>
+            <h2 v-for="(label, numRecords) in $store.state.StatsUser.numRecordsByLabel" :key="label">
+                Number of Records By Label ({{ label }}): {{ numRecords }}
+            </h2>
+            <h2> Total Users: {{ this.$store.state.StatsUser.totalUsers }}</h2>
+            <h2> Total Records: {{ this.$store.state.StatsUser.totalRecords }}</h2>
+            <h2 v-for="(year, numLabels) in $store.state.StatsUser.numRecordsByYear" :key="year">
+                Number of Records By Label ({{ year }}): {{ numLabels }}
+            </h2>
+            <h2 v-for="(country, numLabels) in $store.state.StatsUser.numRecordsByCountry" :key="country">
+                Number of Records By Label ({{ country }}): {{ numLabels }}
+            </h2>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+h2 {
+    color: white;
+}
+
+.AggData {
+    background: black;
+    display: flex;
+}
+.AggDataRight{
+    width: 50vw;
+    background: green;
+}
+</style>
--- vue/src/views/Aliz/AggregateDataTestView.vue
+++ vue/src/views/Aliz/AggregateDataTestView.vue
@@ -1,60 +1,45 @@
 <template>
-    <div class="AggData">
-     <h2> Total Artists: {{ this.$store.state.StatsAggregate.totalArtists}}</h2>
-     <h2> Total Collections: {{ this.$store.state.StatsAggregate.totalCollections}}</h2>
-     <h2> Total Formats: {{ this.$store.state.StatsAggregate.totalFormats}}</h2>
-     <h2> Total Genres: {{ this.$store.state.StatsAggregate.totalGenres}}</h2>
-     <h2> Total Images: {{ this.$store.state.StatsAggregate.totalImages}}</h2>
-     <h2> Total Labels: {{ this.$store.state.StatsAggregate.totalLabels}}</h2>
-     <h2> Total Records in Collections: {{ this.$store.state.StatsAggregate.totalRecordsInCollections}}</h2>
-     <h2> Total Tracks: {{ this.$store.state.StatsAggregate.totalTracks}}</h2>
-     <h2 v-for="(artist, numRecords) in $store.state.StatsAggregate.numRecordsByArtist" :key="artist">
-      Number of Records By Artist ({{ artist }}): {{ numRecords }}
-    </h2>
-    <h2 v-for="(genre, numRecords) in $store.state.StatsAggregate.numRecordsByGenre" :key="genre">
-      Number of Records By Genre ({{ genre }}): {{ numRecords }}
-    </h2>
-    <h2 v-for="(label, numRecords) in $store.state.StatsAggregate.numRecordsByLabel" :key="label">
-      Number of Records By Label ({{ label }}): {{ numRecords }}
-    </h2>
-    <h2> Total Users: {{ this.$store.state.StatsAggregate.totalUsers}}</h2>
-    <h2> Total Records: {{ this.$store.state.StatsAggregate.totalRecords}}</h2>
-    <h2 v-for="(year, numLabels) in $store.state.StatsAggregate.numRecordsByYear" :key="year">
-      Number of Records By Label ({{ year }}): {{ numLabels }}
-    </h2>
-    <h2 v-for="(country, numLabels) in $store.state.StatsAggregate.numRecordsByCountry" :key="country">
-      Number of Records By Label ({{ country }}): {{ numLabels }}
-    </h2>
-
-
-
-</div>
-
+ <aggregate-data-left-components></aggregate-data-left-components>
+ <aggregate-data-right-components v-if="$store.state.token != ''"/>
 </template>
+
 <script>
 
 import StatisticsService from '@/services/StatisticsService.js'
+import AggregateDataLeftComponents from '@/components/AggregateDataLeftComponents.vue'
+import AggregateDataRightComponents from '../../components/AggregateDataRightComponents.vue'
 
 export default {
-methods: {
-    getAggregateForEveryone(){
-        StatisticsService.getAggregateStats()
-        .then(response => {
-            this.$store.commit('ADD_AGGREGATE_STATS', response.data)
-        })
-    }
-},
-created(){
-    this.getAggregateForEveryone();
-}
+    components: {
+        AggregateDataLeftComponents,
+        AggregateDataRightComponents
+    },
+    methods: {
+        getAggregateForEveryone() {
+            StatisticsService.getAggregateStats()
+                .then(response => {
+                    this.$store.commit('ADD_AGGREGATE_STATS', response.data)
+                })
+        },
+        getAggregateForUsers(){
+            StatisticsService.getUserStats()
+            .then(response =>{
+                this.$store.commit('ADD_USER_STATS', response.data)
+            })
+        }
+
+
+
+
+    },
+    created() {
+        this.getAggregateForEveryone(),
+        this.getAggregateForUsers()
+    },
+
+    
 }
 </script>
 
 <style scoped>
-h2{
-    color: white;
-}
-.AggData{
-    background: black;
-}
 </style>
