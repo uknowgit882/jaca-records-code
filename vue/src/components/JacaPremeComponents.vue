740657996794a7d12cf7971c40d33e4a0a42acd9
Aliz Gosztola <aliz.gosztola@gmail.com>
2023-12-12 12:18:58 -0500

    Buttons work , talking to backend

--- vue/src/components/JacaPremeComponents.vue
+++ vue/src/components/JacaPremeComponents.vue
@@ -1,18 +1,67 @@
 <template>
-  <input class="input-users" type="text" placeholder="Text input">
-  <button class="button-deactivate">Deactivate</button>
-  <button class="button-upgrade">Upgrade</button>
-  <button class="button-downgrade">Downgrade</button>
-
-
+  <input class="input-users" type="text" placeholder="Enter Username"
+  v-model="User.username">
+  <button class="button-reactivate" @click="reactivateUser()">Reactivate</button>
+  <button class="button-upgrade" @click="upgradeUser()">Upgrade</button>
+  <button class="button-downgrade" @click="downgradeUser()">Downgrade</button>
 </template>
 <script>
+import JacapremeService from '@/services/JacapremeService.js'
+import PopUpComponent from '@/components/PopUpComponent.vue';
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      User: {
+        username: "",
+      }, 
+    }
+  },
+  methods: {
+    reactivateUser() {
+      JacapremeService.reactivateUser(this.User.username)
+        .then(response => {
+          if (response.status == 200) {
+            console.log(`${this.User.username} was reactivated.`)
+          }
+
+        })
+    },
+    upgradeUser(){
+      JacapremeService.upgradeUserToAdmin(this.User.username)
+      .then(response => {
+        if (response.status == 200){
+          console.log(`${this.User.username} was upgraded.`)
+        }
+      })
+    },
+    downgradeUser(){
+      JacapremeService.downgradeUserFromAdmin(this.User.username)
+      .then(response => {
+        if (response.status == 200){
+          console.log(`${this.User.username} was upgraded.`)
+        }
+      })
+    },
+    alert(){
+      this.alert('Your message')
+      .then(() => {
+        console.log('Alert');
+      })
+    }
+
+
+  }
+}
 
 </script>
 
 <style scoped>
-.input-users{
-    width: 60%;
-    height: 40px;
+.input-users {
+  width: 60%;
+  height: 40px;
 }
 </style>
