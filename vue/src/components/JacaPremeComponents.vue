50d8f5c894ceff6daf3ce6b13022abe69b2cb404
Aliz Gosztola <aliz.gosztola@gmail.com>
2023-12-12 13:30:14 -0500

    pop up button works

--- vue/src/components/JacaPremeComponents.vue
+++ vue/src/components/JacaPremeComponents.vue
@@ -1,7 +1,18 @@
 <template>
-  <input class="input-users" type="text" placeholder="Enter Username"
-  v-model="User.username">
-  <button class="button-reactivate" @click="reactivateUser()">Reactivate</button>
+  <input class="input-users" type="text" placeholder="Enter Username" v-model="User.username">
+
+  <button @click="openup">Reactivate</button>
+  <div v-if="showPopup" class="popup">
+    <div class="popup-inner">
+    </div>
+  </div>
+  <div v-if="showPopup" class="popup">
+    <div class="popup-inner">
+      <h2>You are about to reactivate user : {{ User.username }} </h2>
+      <button class="button-reactivate" @click="reactivateUser()">Reactivate</button>
+      <button class="ExitButton">Exit</button>
+    </div>
+  </div>
   <button class="button-upgrade" @click="upgradeUser()">Upgrade</button>
   <button class="button-downgrade" @click="downgradeUser()">Downgrade</button>
 </template>
@@ -17,40 +28,45 @@ export default {
     return {
       User: {
         username: "",
-      }, 
+      },
+      showPopup: false
     }
   },
   methods: {
+    openup(){
+      this.showPopup = !this.showPopup
+    },
     reactivateUser() {
       JacapremeService.reactivateUser(this.User.username)
         .then(response => {
           if (response.status == 200) {
             console.log(`${this.User.username} was reactivated.`)
+           
           }
 
         })
     },
-    upgradeUser(){
+    upgradeUser() {
       JacapremeService.upgradeUserToAdmin(this.User.username)
-      .then(response => {
-        if (response.status == 200){
-          console.log(`${this.User.username} was upgraded.`)
-        }
-      })
+        .then(response => {
+          if (response.status == 200) {
+            console.log(`${this.User.username} was upgraded.`)
+          }
+        })
     },
-    downgradeUser(){
+    downgradeUser() {
       JacapremeService.downgradeUserFromAdmin(this.User.username)
-      .then(response => {
-        if (response.status == 200){
-          console.log(`${this.User.username} was upgraded.`)
-        }
-      })
+        .then(response => {
+          if (response.status == 200) {
+            console.log(`${this.User.username} was upgraded.`)
+          }
+        })
     },
-    alert(){
+    alert() {
       this.alert('Your message')
-      .then(() => {
-        console.log('Alert');
-      })
+        .then(() => {
+          console.log('Alert');
+        })
     }
 
 
@@ -63,5 +79,37 @@ export default {
 .input-users {
   width: 60%;
   height: 40px;
+}
+
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.popup-inner {
+  background: #fff;
+  padding: 32px;
+  text-align: center;
+}
+
+.popup-inner button {
+  margin-top: 16px;
+  padding: 8px 16px;
+  background-color: #3498db;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+h2{
+  color: black
 }
 </style>
